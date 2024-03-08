02532c31b301cb218c3dee33b48976afd49669ef
Eloy <eloy.campos.davellar@gmail.com>
2024-03-08 11:15:53 +0100

    Actualizando repositorio 0803

---
+++ cli.py
@@ -0,0 +1,15 @@
+import click # It is not the same as CLI, CLICK IS THE LIBRARY.
+
+@click.group()
+def cli():
+     pass
+
+if __name__== '__main__': # It is used to indicate that it is the main module.
+   cli()
+
+
+
+
+
+
+
