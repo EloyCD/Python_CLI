eda3a74f2e6841aea06b23c920cfba07ffeea5b8
Eloy <eloy.campos.davellar@gmail.com>
2024-03-08 11:29:47 +0100

    Actualizando repositorio x2

--- cli.py
+++ cli.py
@@ -7,6 +7,9 @@ def cli():
 if __name__== '__main__': # It is used to indicate that it is the main module.
    cli()
 
+@cli.command() # The command will read the json file.
+def videogames():
+
 
 
 
