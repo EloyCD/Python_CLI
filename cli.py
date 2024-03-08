0f0ee3d56fc77f63eff75cb5a32d263a2e95c0a5
Eloy <eloy.campos.davellar@gmail.com>
2024-03-08 11:49:07 +0100

    Actualizando repositorio x3

--- cli.py
+++ cli.py
@@ -1,14 +1,19 @@
 import click # It is not the same as CLI, CLICK IS THE LIBRARY.
+import json_settings
 
 @click.group()
 def cli():
      pass
 
+@cli.command() # The command will read the json file.
+def videogames():
+    videogames = json_settings.read_json()
+    print(videogames)
+
 if __name__== '__main__': # It is used to indicate that it is the main module.
    cli()
 
-@cli.command() # The command will read the json file.
-def videogames():
+
 
 
 
