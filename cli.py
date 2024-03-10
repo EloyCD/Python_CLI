190721c046af0b0c3ba748b750bbf1a30a41e65c
Eloy <eloy.campos.davellar@gmail.com>
2024-03-10 16:24:00 +0100

    Actualizando repositorio 1003

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--name', required=True, help="Name of game")
+@click.option('--name', required=True, help="Name of game") # option need --, is the same that argument.
 @click.option('--genre', required=True, help="Genre of game")
 @click.option('--platform', required=True, help="Type of platform")
 @click.option('--release_year', required=True, help="Release year of game")
@@ -32,6 +32,18 @@ def videogames():
     for game in videogames:
         print(f"{game['name']} - {game['genre']} - {game['platform']} - {game['release_year']}")
 
+@cli.command()
+@click.argument('name') # argument don't need --, is the same that option.
+def game(name):
+    videogames = json_settings.read_json()
+    # The function x for x in videogames, if the element that you are traversing with its property name == name that we are receiving, will give us the concrete videogame, otherwise it will give None.
+    game = next((x for x in videogames if x ['name'] == name ), None)# Next() is used to sequentially access each element of our list.
+    if game is None:
+        print(f"The game {name} was not found")
+    else:
+        print(f"{game['name']} - {game['genre']} - {game['platform']} - {game['release_year']}")
+
+                                                                
 if __name__ == '__main__':
     cli()
 
