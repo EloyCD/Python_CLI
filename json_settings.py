33bbe3203f55a6a625e1cacc0ecc97ee00317e77
Eloy <eloy.campos.davellar@gmail.com>
2024-03-11 16:41:53 +0100

    Actualizando repo

--- json_settings.py
+++ json_settings.py
@@ -4,7 +4,7 @@ import os # Module interacts with the operating system, in this case, we are goi
 
 def read_json():
     if not os.path.isfile('videogames.json'):  # If the json file does not exist, I want you to create it for me.
-        with open('videogames.json' , 'w') as f:
+        with open('videogames.json' , 'w') as f: # file is always close.
             json.dump([], f) # It would be the object videogames.json and writes it to the file videogames.json
     with open('videogames.json', 'r') as f:
         videogames = json.load(f)
